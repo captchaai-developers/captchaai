947ad7f9cc51a093fece5ccb7b376c7b3bdc0478
captchaai-developers <137160307+captchaai-developers@users.noreply.github.com>
2023-07-11 09:33:44 +0300

    Update recaptcha_example.py

--- tests/recaptcha_example.py
+++ tests/recaptcha_example.py
@@ -4,21 +4,9 @@ from selenium import webdriver
 import sys
 import os
 from captchaai import CaptchaAI
-
-
-# in this example we store the API key inside environment variables that can be set like:
-# export APIKEY_CAPCHAAI=1abc234de56fab7c89012d34e56fa7b8 on Linux or macOS
-# set APIKEY_CAPCHAAI=1abc234de56fab7c89012d34e56fa7b8 on Windows
-# you can just set the API key directly to it's value like:
-# api_key="1abc234de56fab7c89012d34e56fa7b8"
-
-api_key = '238f195955911ee1644dbe88473f02c3'
-your_site_with_captcha = 'https://squareblogs.net/signup'
-
-
+api_key = 'your_api_key'
 solver = CaptchaAI(api_key)
-
-url = your_site_with_captcha
+url = 'https://site/with/normalcaptcha'
 driver = webdriver.Chrome()
 driver.get(url)
 s = driver.find_element(By.XPATH, '//div[@class="g-recaptcha"]')
