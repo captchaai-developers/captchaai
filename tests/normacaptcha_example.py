51b700141263568c955f6d1f99f83e3110ffab92
captchaai-developers <137160307+captchaai-developers@users.noreply.github.com>
2023-07-11 09:32:53 +0300

    Update normacaptcha_example.py

--- tests/normacaptcha_example.py
+++ tests/normacaptcha_example.py
@@ -3,37 +3,14 @@ from selenium import webdriver
 from captchaai import CaptchaAI
 from time import sleep
 
-api_key = '238f195955911ee1644dbe88473f02c3'
+api_key = 'your_api_key'
 solver = CaptchaAI(api_key)
-
-url = 'https://2captcha.com/demo/normal'
-
-
-
-
-
-
-
-
-
-
-
+url = 'https://site/with/normalcaptcha'
 driver = webdriver.Chrome()
 driver.get(url)
 s = driver.find_element(By.XPATH, '//img[contains(@alt,"captcha")]')
 path_to_image = s.get_attribute('src')
-
 result = solver.normal(path_to_image, module='common-1')
-
-
-
-
-
-
-
-
-
-
 WE = driver.find_element(By.XPATH, '//input[@id="simple-captcha-field"]')
 print('Result=', result)
 if result:
